65f5e5c820cccadb5a8f52703f6fd292cfedcddc
hakan poyraz <hakan.poyraz1994@gmail.com>
2018-05-18 10:27:01 +0200

    Maj fait le 18.05.2018 à 10.27

---
+++ Serie_02/Exo_02/script.js
@@ -0,0 +1,8 @@
+function surfaceCercle() {
+  var stringRayon = prompt("Quelle est le rayon du cercle ?")
+  var rayon = Number(stringRayon)
+
+  var resultat = Math.PI*rayon*rayon
+  alert(resultat);
+
+}
